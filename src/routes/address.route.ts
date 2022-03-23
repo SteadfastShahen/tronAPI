f987b1106dde5f71dc158b892c19704efc0a58b6
Shahen <shahen@steadfast.tech>
2022-03-23 16:36:38 +0400

    create account and get balance endpoints added

--- src/routes/address.route.ts
+++ src/routes/address.route.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express'
-const sdk = require('api')('@tron/v4.4.2#fs30lsl0ufmaoh');
+const sdk = require('api')('@tron/v4.4.2#fs30lsl0ufmaoh')
 
 const router = Router()
 
-router.get('/:address', ( req, res ) => {
-    res.send('Get Balance Route')
+router.get('/:address', async ( req, res ) => {
+    const { address } = req.params
+    const validation = await sdk.validateaddress({ address })
+    
+    if(validation?.result){
+        const info = await sdk[ 'account-getaccount' ]({ address })
+        res.send( info )
+    }
+    else{
+        res.send( { msg: 'Invalid' } )
+    }
 })
 
 router.post('/create', async ( req: any, res: any ) => {
@@ -16,9 +25,11 @@ router.post('/create', async ( req: any, res: any ) => {
         console.log(validation)
         const account = await sdk['account-createaccount']({
             owner_address: '41d1e7a6bc354106cb410e65ff8b181c600ff14292',
-            account_address: data.hexAddress
+            account_address: data.hexAddress,
+            visible: false
         })
         res.send( account )
+        // account.raw_data.contract.value.account_address
     } catch ( err ) {
         res.status(400).send( err )
     }
